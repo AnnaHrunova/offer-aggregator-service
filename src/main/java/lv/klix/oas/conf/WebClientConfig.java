26764c3495e186723622ef59984299500797b274
anna <anna.hrunova93@gmail.com>
2025-06-08 08:12:02 +0300

    Add logging interceptors for WebClient

--- src/main/java/lv/klix/oas/conf/WebClientConfig.java
+++ src/main/java/lv/klix/oas/conf/WebClientConfig.java
@@ -1,21 +1,26 @@
 package lv.klix.oas.conf;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
 import org.springframework.http.client.reactive.ReactorClientHttpConnector;
+import org.springframework.web.reactive.function.client.ExchangeFilterFunction;
 import org.springframework.web.reactive.function.client.WebClient;
+import reactor.core.publisher.Mono;
 import reactor.netty.http.client.HttpClient;
 
 import java.time.Duration;
 
 @Configuration
+@Slf4j
 public class WebClientConfig {
 
     @Bean
     public WebClient fastBankWebClient() {
         HttpClient httpClient = HttpClient.create()
+                .wiretap(true)
                 .responseTimeout(Duration.ofSeconds(1));
         return WebClient.builder()
                 .baseUrl("https://shop.stage.klix.app/api/FastBank")
@@ -30,5 +35,18 @@ public class WebClientConfig {
                 .baseUrl("https://shop.stage.klix.app/api/SolidBank")
                 .defaultHeader(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE)
                 .build();
+    }
+
+    ExchangeFilterFunction logRequest() {
+        return ExchangeFilterFunction.ofRequestProcessor(clientRequest -> {
+            if (log.isDebugEnabled()) {
+                StringBuilder sb = new StringBuilder("Request: \n");
+                clientRequest
+                        .headers()
+                        .forEach((name, values) -> values.forEach(value -> sb.append(name).append(": ").append(value).append("\n")));
+                log.debug(sb.toString());
+            }
+            return Mono.just(clientRequest);
+        });
     }
 }
